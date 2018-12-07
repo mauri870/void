e3a89d59787376fcb81bd933573619a99e26a795
Mauri de Souza Nunes <mauri870@gmail.com>
2018-12-06 23:25:51 -0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Void
+
+Send data into the void. That's it. All data passed by stdin will be gone, forever.
+
+## Compilation
+
+```bash
+cargo build
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use std::io;
 
+extern crate argparse;
+use argparse::{ArgumentParser, StoreTrue};
+
 fn main(){
-    io::copy(&mut io::stdin(), &mut io::sink()).unwrap();
+    let mut verbose = false;
+
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Send data into the void");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue,
+            "Enable verbose mode");
+        ap.parse_args_or_exit();
+    }
+
+    let n = io::copy(&mut io::stdin(), &mut io::sink()).unwrap();
+    if verbose {
+        eprintln!("{} bytes send into the void", n)
+    }
 }
